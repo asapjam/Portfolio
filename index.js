bd18cab37463cf6018d8ad4761c248553d67ea79
asapjam <jameslawson9k@gmail.com>
2026-09-13 11:21:02 -0600

    Javascript DOM 1

---
+++ index.js
@@ -0,0 +1,43 @@
+const projectItems = [
+    {    
+        title: "My First Project",
+        url: "http://link.to.my.first.project.com",
+        description: "My first project is the first project I ever made, and it's cool because...",
+    },
+    {    
+        title: "My First Project",
+        url: "http://link.to.my.first.project.com",
+        description: "My first project is the first project I ever made, and it's cool because...",
+    },
+    {    
+        title: "My First Project",
+        url: "http://link.to.my.first.project.com",
+        description: "My first project is the first project I ever made, and it's cool because...",
+    },
+];
+
+const projectTargetDiv = document.getElementById("projectTarget");
+
+projectItems.forEach(projectItem => {
+    const titleText = projectItem.title;
+    const urlText = projectItem.url;
+    const descriptionText = projectItem.description;
+
+    const newTitleDiv = document.createElement("div");
+    newTitleDiv.classList.add("projectTitle");
+    newTitleDiv.textContent = titleText;
+
+    const newUrlDiv = document.createElement("div");
+    newUrlDiv.classList.add("projectUrl");
+    newUrlDiv.textContent = "<a href =" + urlText + "></a>";
+
+    const newDescriptionDiv = document.createElement("div");
+    newDescriptionDiv.classList.add("projectDescription");
+    newDescriptionDiv.textContent = descriptionText;
+
+    projectTargetDiv.appendChild(newTitleDiv);
+    projectTargetDiv.appendChild(newUrlDiv);
+    projectTargetDiv.appendChild(newDescriptionDiv);
+
+
+})
